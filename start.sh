9bf3d3ce9de201cbb0668ff79834acbf56ee1ad0
Nolan Jannotta <anjannotta@verizon.net>
2025-03-29 23:19:56 -0700

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 
-docker run -d -p 2227:2227 -v ~/.ssh/chat-app:/root/.ssh/ ssh-chat
+sudo docker run -d -p 2227:2227 -v ~/.ssh/chat-app:/root/.ssh/ ssh-chat
+sudo docker system prune -af
