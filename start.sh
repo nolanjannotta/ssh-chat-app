45d8f807258a29a4d46d1dbfeebed4102bd6f1b7
Nolan Jannotta <anjannotta@verizon.net>
2025-04-05 12:07:57 -0700

    start script

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+
+
 sudo docker build -t ssh-chat .
+
+containerId = $(sudo docker ps --filter "ancestor=ssh-chat" --format "{{.ID}}")
+sudo docker stop $containerId
+
 sudo docker run -d -p 2227:2227 -v ~/.ssh/chat-app:/root/.ssh/ ssh-chat
 sudo docker system prune -af
